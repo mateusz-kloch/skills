417ad048af92b0d5a23aac68a1233ff291d93d93
mateusz-kloch <mateusz.kloch@o2.pl>
2024-04-16 15:33:11 +0200

    Update `create_user()` func

---
+++ tests/utils.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
+
+from api.serializers import ArticleSerializer, UserSerializer
+from django_articles.models import Article, Tag
+
+
+def create_user(name: str, password: str) -> User:
+    """
+    Creates an Author model object.
+    """
+    return User.objects.create_user(username=name, email=f'{name}@example.com', password=password)
+
+
+def create_tag(name: str) -> Tag:
+    """
+    Creates a Tag model object.
+    """
+    return Tag.objects.create(name=name)
+
+
+def create_article(title: str, author: User, tags: list[Tag], pub_date: datetime, content: str) -> Article:
+    """
+    Creates an article model object and establishes a many-to-many relation with the given Tag objects.
+    """
+    article = Article(
+        title=title,
+        author=author,
+        pub_date=pub_date,
+        content=content
+    )
+    article.save()
+    article.tags.set(tags)
+    return article
+
+
+def serialize_article_with_absolute_urls(article: Article):
+    serialized_article = ArticleSerializer(article, context={'request': None}).data
+    serialized_article['url'] = f'http://testserver{serialized_article["url"]}'
+    serialized_article['author'] = f'http://testserver{serialized_article["author"]}'
+    serialized_article['tags'] = [f'http://testserver{tag}' for tag in serialized_article['tags']]
+    return serialized_article
+
+
+def serialize_user_with_absolute_urls(user: User):
+    serialized_user = UserSerializer(user, context={'request': None}).data
+    serialized_user['url'] = f'http://testserver{serialized_user["url"]}'
+    serialized_user['articles'] = [f'http://testserver{article}' for article in serialized_user['articles']]
+    return serialized_user
