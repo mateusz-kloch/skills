84c722c8c8e95c11dfc55d7cb92f1ea57755953c
mateusz-kloch <mateusz.kloch@o2.pl>
2024-04-24 12:46:22 +0200

    Move tests for create user to test_api_users_endpoints.py

--- tests/test_api_users_endpoint.py
+++ tests/test_api_users_endpoint.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -25,6 +26,33 @@ class ApiUserListTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['count'], 0)
         self.assertEqual(response.data['results'], [])
+
+
+    def test_post_user_list_new_user(self):
+        """
+        Checks register response for create new user.
+        """
+        user_data = {
+            'username': 'author',
+            'password': 'woeiuhtg9823y'
+        }
+        register = self.client.post('/api/users/', user_data)
+        new_user = User.objects.get(username=user_data['username'])
+        self.assertEqual(register.status_code, status.HTTP_201_CREATED)
+        self.assertQuerySetEqual(User.objects.all(), [new_user])
+
+
+    def test_post_user_list_new_user_bad_password(self):
+        """
+        Checks register response for create new user with poor password.
+        """
+        user_data = {
+            'username': 'author',
+            'password': 'password'
+        }
+        register = self.client.post('/api/users/', user_data)
+        self.assertEqual(register.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertQuerySetEqual(User.objects.all(), [])
 
 
 class ApiUserDetailTests(APITestCase):
