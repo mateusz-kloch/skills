4243853e615320a3eb9695cb5e281f5174dc7d8a
mateusz-kloch <mateusz.kloch@o2.pl>
2024-05-07 12:35:53 +0200

    Update test

--- skills_backend/library/tests/test_api_root.py
+++ skills_backend/library/tests/test_api_root.py
@@ -5,7 +5,7 @@ from rest_framework.test import APITestCase
 class ApiRootTests(APITestCase):
     def test_get_api_root_response_status_code(self):
         """
-        Checks API root response status code.
+        Checks API root response status code for unauthenticated user.
         """
         url = '/api/'
         response = self.client.get(url)
