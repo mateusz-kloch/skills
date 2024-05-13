4ef801dbaab650c89ab61bb32b924b8d956b04d9
mateusz-kloch <mateusz.kloch@o2.pl>
2024-05-13 13:21:23 +0200

    Update pagnation

--- skills_backend/core/settings.py
+++ skills_backend/core/settings.py
@@ -135,7 +135,7 @@ INTERNAL_IPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',),
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 15,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 'PAGE_SIZE': 9,
 }
 
 # IsAuthenticatedOrReadOnly
