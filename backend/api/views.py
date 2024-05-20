df9d63bacc3c27233cee3c66f53b2c4cd88d5d5a
mateusz-kloch <mateusz.kloch@o2.pl>
2024-05-20 13:53:04 +0200

    Include new Author model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,22 +1,26 @@
-from django.contrib.auth.models import User
 from rest_framework import mixins
 from rest_framework import permissions
 from rest_framework import viewsets
 
-from api.permissions import IsAnonymousOrNotAllowed ,IsOwnerOrReadOnly
-from api.serializers import ArticleSerializer, TagSerializer, UserSerializer
+from api.serializers import AuthorSerializer, ArticleSerializer, TagSerializer
+from common.permissions import IsAnonymousOrNotAllowed ,IsOwnerOrReadOnly
+from accounts.models import Author
 from library.models import Article, Tag
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     """
-    Allows anyone to display published articles and to create a new article by logged in User.
+    Allows anyone to display published articles and
+    to create a new article by logged in User.
     
-    Uses custom permission `IsOwnerOrReadOnly` that ensures only author of article can edit it.
+    Uses custom permission `IsOwnerOrReadOnly` that ensures only
+    author of article is allowed to edit it.
     """
     queryset = Article.verified_objects.all()
     serializer_class = ArticleSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly
+    ]
 
     def perform_create(self, serializer):
         """
@@ -33,12 +37,19 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = TagSerializer
 
 
-class UserViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin, viewsets.GenericViewSet):
+class AuthorViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
     """
-    Allows anyone to display users and create a new account by anonymous visitors.
+    Allows anyone to display users and
+    to create a new account by anonymous visitors.
 
-    Uses custom permission `IsAnonOrNotAllowed` that ensures only not loggen in user can create account.
+    Uses custom permission `IsAnonymousOrNotAllowed` that ensures only
+    not logged in user can create account.
     """
-    queryset = User.objects.all().order_by('username')
-    serializer_class = UserSerializer
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
     permission_classes = [IsAnonymousOrNotAllowed]
