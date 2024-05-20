368e8656c26c4bebc00e7134e180c7cecda9796a
mateusz-kloch <mateusz.kloch@o2.pl>
2024-05-20 13:54:58 +0200

    Include new `Author` model

--- backend/library/admin.py
+++ backend/library/admin.py
@@ -3,26 +3,20 @@ from django.contrib import admin
 from library.models import Article, Tag
 
 
-class ArticleAdmin(admin.ModelAdmin):
+class ArticleAdminConfig(admin.ModelAdmin):
     """
-    Creates custom view of articles for an admin.
+    A custom view of articles for an admin.
     """
+    search_fields = ('title', 'author')
     fieldset = [('Article informations', {'fields': [
-        'title',
-        'author',
-        'tags',
-        'pub_date',
-        'content'
+        'title', 'author', 'tags', 'pub_date', 'content'
     ]})]
     filter_horizontal = ['tags']
     list_display = [
-        'title',
-        'author',
-        'pub_date',
-        'tags_as_str'
+        'title', 'author', 'pub_date', 'tags_as_str'
     ]
     list_filter = ['author', 'pub_date', 'tags']
 
 
-admin.site.register(Article, ArticleAdmin)
+admin.site.register(Article, ArticleAdminConfig)
 admin.site.register(Tag)
