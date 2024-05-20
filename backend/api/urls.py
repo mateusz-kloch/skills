4cc6d1080e87994bc893b9c5dde3dcc002e8cb58
mateusz-kloch <mateusz.kloch@o2.pl>
2024-05-20 13:52:20 +0200

    Include new Author model

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,15 +4,14 @@ from rest_framework.routers import DefaultRouter
 from api.views import (
     ArticleViewSet,
     TagViewSet,
-    UserViewSet
+    AuthorViewSet
 )
 
-
 router = DefaultRouter()
+router.register(r'authors', AuthorViewSet)
 router.register(r'articles', ArticleViewSet)
-router.register(r'users', UserViewSet)
 router.register(r'tags', TagViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
 ]
