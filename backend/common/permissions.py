9d5f4ebdd6a8e35e1d005eb97763c3777d468fc6
mateusz-kloch <mateusz.kloch@o2.pl>
2024-05-29 16:18:54 +0200

    Update permissions

--- backend/common/permissions.py
+++ backend/common/permissions.py
@@ -3,21 +3,38 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Ensures that only author of an object can manage it.
+    """
     def has_object_permission(self, request, view, obj):
-        """
-        Ensures that only author of an article can edit it.
-        """
         if request.method in permissions.SAFE_METHODS:
             return True
         else:
             return obj.author == request.user
         
 
-class IsAnonymousOrNotAllowed(permissions.BasePermission):
+class IsStaffOrReadOnly(permissions.BasePermission):
+    """
+    Ensures that only staff user can create, edit or delete the object.
+    """
     def has_permission(self, request, view):
-        """
-        Ensures that only anonymous visitors can create a new account.
-        """
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return request.user.is_staff
+        
+
+class IsAnonymousOrNotAllowed(permissions.BasePermission):
+    """
+    Ensures that only anonymous visitors can create a new account.
+    """
+    def has_permission(self, request, view):
         if request.method == 'POST':
             return isinstance(request.user, AnonymousUser)
         else:
