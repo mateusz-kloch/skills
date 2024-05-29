c79ee483de7769b06c248b80ef1231db1b9ea71d
mateusz-kloch <mateusz.kloch@o2.pl>
2024-05-29 14:11:23 +0200

    Remove custom models managers: `app/models.py` -> `common/selectors.py`

---
+++ backend/common/selectors.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.models import BaseUserManager
+from django.db import models
+from django.utils import timezone
+
+
+class CustomArticleManager(models.Manager):
+    """
+    A custom `Article` model manager.
+    """
+    def get_queryset(self) -> models.QuerySet:
+        """
+        Filters out defective articles.
+        """
+        return super().get_queryset().filter(
+        pub_date__lte=timezone.now()
+        ).exclude(
+            title=''
+        ).exclude(
+            author__isnull=True
+        ).exclude(
+            tags__isnull=True
+        ).exclude(
+            content=''
+        )
+
+
+class CustomAccountManager(BaseUserManager):
+
+    def create_superuser(self, user_name, email, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must be assigned to `is_superuser=True`.')
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to `is_staff=True`.')
+        
+        return self.create_user(user_name, email, password, **other_fields)
+    
+    def create_user(self, user_name, email, password, **other_fields):
+        email = self.normalize_email(email)
+
+        user = self.model(user_name=user_name, email=email, **other_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
--- backend/library/models.py
+++ backend/library/models.py
@@ -3,23 +3,10 @@ from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
+from common.selectors import CustomArticleManager
+
 
 class Article(models.Model):
-
-    class VerifiedArticles(models.Manager):
-        """
-        Filters out defective articles.
-        """
-        def get_queryset(self) -> models.QuerySet:
-            return super().get_queryset().filter(
-            pub_date__lte=timezone.now()
-        ).exclude(
-            title=''
-        ).exclude(
-            tags__isnull=True
-        ).exclude(
-            content=''
-        )
 
     class Meta:
         ordering = ['-pub_date']
@@ -32,7 +19,7 @@ class Article(models.Model):
     content = models.TextField()
 
     objects = models.Manager()
-    verified_objects = VerifiedArticles()        
+    verified_objects = CustomArticleManager()
 
     def __str__(self):
         return self.title
