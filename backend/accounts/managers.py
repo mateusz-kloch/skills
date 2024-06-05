98f90259f6644d2c582dcfef732851a8896aaba3
mateusz-kloch <mateusz.kloch@o2.pl>
2024-06-05 14:17:03 +0200

    Remove `CustomAccountManager`: `backend/common/selectors.py` -> `backend/accounts/managers.py`

--- backend/accounts/managers.py
+++ backend/accounts/managers.py
@@ -1,27 +1,4 @@
 from django.contrib.auth.models import BaseUserManager
-from django.db import models
-from django.utils import timezone
-
-
-class CustomArticleManager(models.Manager):
-    """
-    A custom `Article` model manager.
-    """
-    def get_queryset(self) -> models.QuerySet:
-        """
-        Filters out defective articles.
-        """
-        return super().get_queryset().filter(
-        pub_date__lte=timezone.now()
-        ).exclude(
-            title=''
-        ).exclude(
-            author__isnull=True
-        ).exclude(
-            tags__isnull=True
-        ).exclude(
-            content=''
-        )
 
 
 class CustomAccountManager(BaseUserManager):
