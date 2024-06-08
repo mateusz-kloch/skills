5e5adcd1f815a2b0f9b2f329f267a0c7eca46551
mateusz-kloch <mateusz.kloch@o2.pl>
2024-06-08 14:32:17 +0200

    Implement regular expressions for detail views

--- backend/library/urls.py
+++ backend/library/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import TemplateView
 
 from .views import (
@@ -16,9 +16,9 @@ app_name = 'library'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='library/index.html'), name='index'),
     path('articles/', ArticleListView.as_view(), name='article-list'),
-    path('articles/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    re_path(r'^(?:article-(?P<pk>[0-9]+))/$', ArticleDetailView.as_view(), name='article-detail'),
     path('authors/', AuthorListView.as_view(), name='author-list'),
-    path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
+    re_path(r'^(?:author-(?P<pk>[0-9]+))/$', AuthorDetailView.as_view(), name='author-detail'),
     path('tags/', TagListView.as_view(), name='tag-list'),
-    path('tags/<int:pk>/', TagDetailView.as_view(), name='tag-detail'),
+    re_path(r'^(?:tag-(?P<pk>[0-9]+))/$', TagDetailView.as_view(), name='tag-detail'),
 ]
