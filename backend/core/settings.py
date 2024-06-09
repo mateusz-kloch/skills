4c8430fc5f4e6c7f398c840547c6820ad80d4e2f
mateusz-kloch <mateusz.kloch@o2.pl>
2024-06-09 13:00:23 +0200

    Disable debug

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--&yb3amjo*nv)zj*$791tt!ddr#_)*j0h^w%$fda@&qyi-xk19'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
