7839adb4ff2823c4ffca7dfff643d523813017de
mateusz-kloch <mateusz.kloch@o2.pl>
2024-06-12 16:55:39 +0200

    Add paths to register, login and logout users

--- backend/library/urls.py
+++ backend/library/urls.py
@@ -1,13 +1,15 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path, re_path
 from django.views.generic import TemplateView
 
 from .views import (
-    ArticleListView,
-    AuthorListView,
-    AuthorDetailView,
     ArticleDetailView,
-    TagListView,
+    ArticleListView,
+    AuthorDetailView,
+    AuthorListView,
     TagDetailView,
+    TagListView,
+    UserRegisterView,
 )
 
 
@@ -21,4 +23,7 @@ urlpatterns = [
     re_path(r'^(?:author-(?P<slug>[0-9-a-z]+))/$', AuthorDetailView.as_view(), name='author-detail'),
     path('tags/', TagListView.as_view(), name='tag-list'),
     re_path(r'^(?:tag-(?P<slug>[0-9-a-z]+))/$', TagDetailView.as_view(), name='tag-detail'),
+    path('register/', UserRegisterView.as_view(), name='user-register'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'library/user_login.html', next_page = 'library:index'), name='user-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'library/user_after_logout.html'), name='user-logout'),
 ]
