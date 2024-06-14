10fdb9ea0a600d4b31ae93779525a7f54d75dd57
mateusz-kloch <mateusz.kloch@o2.pl>
2024-06-14 10:53:10 +0200

    Refactory paths

--- backend/library/urls.py
+++ backend/library/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path, re_path
-from django.views.generic import TemplateView
+from django.views import generic
 
 from .views import (
     ArticleDetailView,
@@ -16,14 +16,40 @@ from .views import (
 app_name = 'library'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='library/index.html'), name='index'),
-    path('articles/', ArticleListView.as_view(), name='article-list'),
-    re_path(r'^(?:article-(?P<slug>[0-9-a-z]+))/$', ArticleDetailView.as_view(), name='article-detail'),
-    path('authors/', AuthorListView.as_view(), name='author-list'),
-    re_path(r'^(?:author-(?P<slug>[0-9-a-z]+))/$', AuthorDetailView.as_view(), name='author-detail'),
-    path('tags/', TagListView.as_view(), name='tag-list'),
-    re_path(r'^(?:tag-(?P<slug>[0-9-a-z]+))/$', TagDetailView.as_view(), name='tag-detail'),
-    path('register/', UserRegisterView.as_view(), name='user-register'),
-    path('login/', auth_views.LoginView.as_view(template_name = 'library/user_login.html', next_page = 'library:index'), name='user-login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'library/user_logout.html'), name='user-logout'),
+    path(
+        '', generic.TemplateView.as_view(
+            template_name='library/index.html'
+        ), name='index'
+    ),
+    path(
+        'articles/', ArticleListView.as_view(), name='article-list'
+    ),
+    re_path(
+        r'^(?:article-(?P<slug>[0-9-a-z]+))/$', ArticleDetailView.as_view(), name='article-detail'
+    ),
+    path(
+        'authors/', AuthorListView.as_view(), name='author-list'
+    ),
+    re_path(
+        r'^(?:author-(?P<slug>[0-9-a-z]+))/$', AuthorDetailView.as_view(), name='author-detail'
+    ),
+    path(
+        'tags/', TagListView.as_view(), name='tag-list'
+    ),
+    re_path(
+        r'^(?:tag-(?P<slug>[0-9-a-z]+))/$', TagDetailView.as_view(), name='tag-detail'
+    ),
+    path(
+        'register/', UserRegisterView.as_view(), name='user-register'
+    ),
+    path(
+        'login/', auth_views.LoginView.as_view(
+            template_name = 'library/user_login.html', next_page = 'library:index'
+        ), name='user-login'
+    ),
+    path(
+        'logout/', auth_views.LogoutView.as_view(
+            template_name = 'library/user_logout.html'
+        ), name='user-logout'
+    ),
 ]
